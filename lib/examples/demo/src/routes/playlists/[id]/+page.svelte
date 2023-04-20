29c3b38f92bfbb0ca2e685dac98600c46335bf6c
tzuleger <tzuleger@3riverstech.com>
2023-04-20 00:08:57 -0500

    Fixed up where so it properly works with included models as well as properly typed.

--- lib/examples/demo/src/routes/playlists/[id]/+page.svelte
+++ lib/examples/demo/src/routes/playlists/[id]/+page.svelte
@@ -3,34 +3,32 @@
 
 </script>
 
-<div class="container">
-    <label class="form-label">Playlist ID<input class="form-control" disabled value={data.playlist.PlaylistId}></label>
-    <label class="form-label">Playlist Name<input class="form-control" bind:value={data.playlist.Name}></label>
-    
-    <br>Tracks
-    <table class="table table-hover table-striped">
-        <thead>
-            <th>Id</th>
-            <th>Name</th>
-            <th>Composer</th>
-            <th>Duration</th>
-            <th>Bytes</th>
-            <th>Genre</th>
-        </thead>
-        <tbody>
-            {#each data.playlist.PlaylistTracks as pTrack}
-                <tr>
-                    <td>{pTrack.Track.TrackId}</td>
-                    <td>{pTrack.Track.Name}</td>
-                    <td>{pTrack.Track.Composer ?? "UNKNOWN"}</td>
-                    <td>{Math.floor((pTrack.Track.Milliseconds / 1000) / 60)}:{Math.floor((pTrack.Track.Milliseconds / 1000) % 60).toString().padStart(2, '0')}</td>
-                    <td>{(pTrack.Track.Bytes / 1024 / 1024).toFixed(2)}Mb</td>
-                    <td>{pTrack.Track.Genre?.Name}</td>
-                </tr>
-            {/each}
-        </tbody>
-    </table>
-</div>
+<label class="form-label">Playlist ID<input class="form-control" disabled value={data.playlist.PlaylistId}></label>
+<label class="form-label">Playlist Name<input class="form-control" bind:value={data.playlist.Name}></label>
+
+<br>Tracks
+<table class="table table-hover table-striped table-dark">
+    <thead>
+        <th>Id</th>
+        <th>Name</th>
+        <th>Composer</th>
+        <th>Duration</th>
+        <th>Bytes</th>
+        <th>Genre</th>
+    </thead>
+    <tbody>
+        {#each data.playlist.PlaylistTracks as pTrack}
+            <tr>
+                <td>{pTrack.Track.TrackId}</td>
+                <td>{pTrack.Track.Name}</td>
+                <td>{pTrack.Track.Composer ?? "UNKNOWN"}</td>
+                <td>{Math.floor((pTrack.Track.Milliseconds / 1000) / 60)}:{Math.floor((pTrack.Track.Milliseconds / 1000) % 60).toString().padStart(2, '0')}</td>
+                <td>{(pTrack.Track.Bytes / 1024 / 1024).toFixed(2)}Mb</td>
+                <td>{pTrack.Track.Genre?.Name}</td>
+            </tr>
+        {/each}
+    </tbody>
+</table>
 
 <style>
     tr {
